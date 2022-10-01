96b6d824dc07f4da3767279115f5f49d1601700f
deividadrian <deividadrian24@gmail.com>
2022-10-01 15:18:55 -0300

    Configurando a conexão Banco de dados

--- src/data-sourse.ts
+++ src/data-sourse.ts
@@ -1,10 +1,15 @@
+import 'dotenv/config';
+import 'reflect-metadata';
 import { DataSource } from "typeorm";
+
+const port = process.env.DB_PORT as number | undefined;
 
 export const AppDataSourse = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST,
-  port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "componentes"
-})
+  port: port,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+ 
